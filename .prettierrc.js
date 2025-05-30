0a652e7ef03d4061fcc1cbaa9adfb0ab1e59c7f7
mikalengelo64 <donni2001331@gmail.com>
2025-05-30 17:10:35 +0300

    [OQ-1] feat: init, set up eslint config

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  useTabs: false,
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+
+export default config;
---
+++ eslint.config.mjs
@@ -0,0 +1,76 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  { ignores: ['build'] },
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+  }),
+];
+
+export default tseslint.config(
+  ...eslintConfig,
+
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-empty-object-type': 0,
+      '@typescript-eslint/no-floating-promises': 0,
+      '@typescript-eslint/restrict-plus-operands': 0,
+      '@typescript-eslint/naming-convention': [
+        'error',
+        { selector: 'typeAlias', format: ['PascalCase'], prefix: ['T'] },
+        { selector: 'interface', format: ['PascalCase'], prefix: ['I'] },
+        { selector: 'enum', format: ['PascalCase'], prefix: ['E'] },
+        {
+          selector: ['variable', 'function', 'parameter'],
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase', 'snake_case'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: ['variable'],
+          types: ['boolean'],
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+          prefix: ['is', 'IS', 'Is', 'has', 'HAS', 'Has', 'can', 'CAN', 'Can'],
+        },
+      ],
+    },
+  },
+  // fix esling.config.mjs error
+  {
+    files: [
+      '**/eslint.config.mjs',
+      '**/.prettierrc.js',
+      '**/postcss.config.mjs',
+    ],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
+);
