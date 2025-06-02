611e06c1e9f296df14e1be2c862bf9d6bc1f090e
mikalengelo64 <donni2001331@gmail.com>
2025-06-02 08:22:59 +0300

    [OQ-12] feat: styled main page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ import {
 } from '@/data/staticDataUtils';
 import { fetchScreensData } from '@/lib/features/questionnaire/server/fetchScreensData';
 import { EExtremeStatus, TScreen } from '@/types/question.type';
+import Button from '@/components/Button/Button';
+
+import styles from './styles.module.scss';
 
 async function getFirstScreenData(): Promise<{
   screenData: TScreen | null;
@@ -33,16 +36,20 @@ export default async function Home() {
   const { screenData } = await getFirstScreenData();
 
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <p>Let&apos;s start test!</p>
+    <>
+      <main className={styles.content}>
+        <h1 className={styles.title}>Let&apos;s start questionnaire!</h1>
 
-        <Link href={screenData?.screenId ?? '/'}>Start test</Link>
+        <Button asChild className={styles.button}>
+          <Link href={screenData?.screenId ?? '/'}>Start test</Link>
+        </Button>
       </main>
 
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
-        footer
+      <footer className={styles.footer}>
+        <p className={styles.footer__description}>
+          Developed by @Mikelangelo64
+        </p>
       </footer>
-    </div>
+    </>
   );
 }
