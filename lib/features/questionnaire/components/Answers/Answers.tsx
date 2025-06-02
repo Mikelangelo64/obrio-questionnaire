c3d358c967767805b042b2e3ccfc0913e86aca3e
mikalengelo64 <donni2001331@gmail.com>
2025-06-02 19:18:50 +0300

    [OQ-19] feat: added text with dynamic segments on the results page

--- lib/features/questionnaire/components/Answers/Answers.tsx
+++ lib/features/questionnaire/components/Answers/Answers.tsx
@@ -4,6 +4,7 @@ import { useAppSelector } from '@/store/hooks';
 import { selectAnswers } from '@/store/slices/questionnaireSlice';
 import cn from 'clsx';
 import styles from './styles.module.scss';
+import TextWithDynamicSegments from '../TextWithDynamicSegments/TextWithDynamicSegments';
 
 const Answers = ({ className }: { className?: string }) => {
   const answers = useAppSelector(selectAnswers);
@@ -12,7 +13,10 @@ const Answers = ({ className }: { className?: string }) => {
     <ul className={cn(styles.answers, className)}>
       {Object.entries(answers).map(([screenId, answer]) => (
         <li key={screenId} className={styles.item}>
-          <p className={styles.title}>{answer.questionTitle}</p>
+          <TextWithDynamicSegments
+            className={styles.title}
+            text={answer.questionTitle}
+          />
 
           <p className={styles.answer}>{answer.value}</p>
         </li>
