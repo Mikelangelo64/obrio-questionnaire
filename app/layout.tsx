c35766c92c9020be8946e273044981a98177433f
mikalengelo64 <donni2001331@gmail.com>
2025-06-02 13:33:40 +0300

    [OQ-1] chore: moved StoreProvider into components folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 import cn from 'clsx';
-import StoreProvider from './StoreProvider';
+import StoreProvider from '../components/StoreProvider';
 import '@/styles/index.scss';
 import ViewportHeightCalculation from '@/lib/features/ViewportHeightCalculation';
 
